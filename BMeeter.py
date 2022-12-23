5af113fda9b827ba6159598d3811ff01cb4b11db
ihl <44904038+ihl7@users.noreply.github.com>
2022-12-23 22:13:23 +0300

    Update BMeeter.py

--- BMeeter.py
+++ BMeeter.py
@@ -1,6 +1,7 @@
-import pymem
+import pymem,time
 while (True):
   try:
     pm = pymem.Pymem("voicemeeter8.exe")
     pm.write_int(pm.process_base.lpBaseOfDll + 0x11FBE8,0)
+    time.sleep(5)
   except: pass
